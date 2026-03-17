33eeafae9781fe1dd9a0b94c9682eaae8c5fd3bc
inesromerooneto <ilromero@miuandes.cl>
2026-03-17 11:43:50 -0300

    avance lectura archivos

---
+++ main.py
@@ -0,0 +1,42 @@
+def leer_tareas(nombre_archivo):
+    tareas = []
+
+    with open(nombre_archivo, "r") as archivo:
+        for linea in archivo:
+            linea = linea.strip()  # sacar saltos de línea
+
+            if linea == "":
+                continue  # saltar líneas vacías
+
+            partes = linea.split(",")
+
+            tarea = {
+                "id": partes[0],
+                "duracion": int(partes[1]),
+                "categoria": partes[2]
+            }
+
+            tareas.append(tarea)
+
+    return tareas
+
+def leer_recursos(nombre_archivo):
+    recursos = []
+
+    with open(nombre_archivo, "r") as archivo:
+        for linea in archivo:
+            linea = linea.strip()
+
+            if linea == "":
+                continue
+
+            partes = linea.split(",")
+
+            recurso = {
+                "id": partes[0],
+                "categorias": partes[1:]  # todas las demás columnas
+            }
+
+            recursos.append(recurso)
+
+    return recursos
