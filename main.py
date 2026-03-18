92f9441dfea19e6617268437a4c70329f0e5a9a8
inesromerooneto <ilromero@miuandes.cl>
2026-03-18 10:12:54 -0300

    Avances: agrego de funciones

--- main.py
+++ main.py
@@ -1,52 +1,68 @@
-from pathlib import Path
-
 def leer_tareas(nombre_archivo):
     tareas = []
-    ruta = Path(__file__).resolve().parent / nombre_archivo
 
-    with open(ruta, "r", encoding="utf-8") as archivo:
+    with open(nombre_archivo, "r") as archivo:
         for linea in archivo:
             linea = linea.strip()  # sacar saltos de línea
 
             if linea == "":
                 continue  # saltar líneas vacías
 
-            partes = [p.strip() for p in linea.split(",")]  # dividir por comas y quitar espacios
+            partes = linea.split(",")
 
             tarea = {
                 "id": partes[0],
                 "duracion": int(partes[1]),
                 "categoria": partes[2]
             }
-            
+
             tareas.append(tarea)
-    
+
     return tareas
-            
+
 def leer_recursos(nombre_archivo):
     recursos = []
-    ruta = Path(__file__).resolve().parent / nombre_archivo
 
-    with open(ruta, "r", encoding="utf-8") as archivo:
+    with open(nombre_archivo, "r") as archivo:
         for linea in archivo:
             linea = linea.strip()
 
             if linea == "":
                 continue
 
-            partes = [p.strip() for p in linea.split(",")]
+            partes = linea.split(",")
 
             recurso = {
                 "id": partes[0],
-                "categorias": partes [1:]  # todas las partes después del ID son categorías 
+                "categorias": partes[1:]  # todas las demás columnas
             }
 
             recursos.append(recurso)
 
     return recursos
 
-tareas = leer_tareas("tareas.txt")
-recursos = leer_recursos("recursos.txt")
+def recursos_compatibles(tarea, recursos):
+    compatibles = []
 
-print("Tareas:", tareas)
-print("Recursos:", recursos)
+    for recurso in recursos:
+        if tarea["categoria"] in recurso["categorias"]:
+            compatibles.append(recurso)
+
+    return compatibles
+
+def crear_disponibilidad(recursos):
+    disponibilidad = {}
+
+    for recurso in recursos:
+        disponibilidad[recurso["id"]] = 0
+
+    return disponibilidad
+
+def elegir_recurso(compatibles, disponibilidad):
+    mejor = compatibles[0]
+
+    for recurso in compatibles:
+        if disponibilidad[recurso["id"]] < disponibilidad[mejor["id"]]:
+            mejor = recurso
+
+    return mejor
